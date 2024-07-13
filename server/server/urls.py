d96b360b74ae6c3b2bec96538971182f1cd21370
Tal Weiss <Tweiss747@gmail.com>
2024-07-12 23:42:03 -0400

    registered user routes and gave notes route name

--- server/server/urls.py
+++ server/server/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/', include('Notes.urls')),
+    path('api/v1/notes/', include('Notes.urls')),
+    path('api/v1/users/', include('Users.urls')),
 ]
