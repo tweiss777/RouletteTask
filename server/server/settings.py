80c68ccffe0f6a0e039c1201ea6e8d67883d79c1
Tal Weiss <Tweiss747@gmail.com>
2024-07-13 16:00:31 -0400

    configured jwt

--- server/server/settings.py
+++ server/server/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-
+from datetime import timedelta
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -125,8 +125,14 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-REST_FRAMEWORK ={
-        'DEFAULT_AUTHENTICATION_CLASSES': {
-            'rest_framework_simplejwt.authentication.JWTAuthentication',
-        }
+# rest framework settings
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
+}
+
+SIMPLE_JWT ={
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
+    'REFRESH_TOKEN_LIFETIME':timedelta(days=1), 
 }
