5532c77cdf1a173b31786b798f992f285fd3c25a
Tal Weiss <Tweiss747@gmail.com>
2024-07-12 23:42:44 -0400

    started implementation of register users

--- server/Users/views.py
+++ server/Users/views.py
@@ -3,11 +3,26 @@ from django.http import HttpResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
-
+from Users.models import Users
 # registers user and returns token
 # todo add exception handling
 @api_view(["POST"])
 def register(request) -> Response:
+    newUser = request.data
+    username, password = newUser["username"], newUser["password"]
+    if username == "" or password == "":
+        return Response({"message": "username or password cannot be empty"}, status=status.HTTP_400_BAD_REQUEST)
+    exists = Users.objects.filter(username=username).exists()
+    if exists:
+        return Response({"message": "username already exists"}, status=status.HTTP_409_CONFLICT)
+
+    Users.objects.create(username=username, password=password)
+    #create the token with the username, email, role
+    tokenClaims = {
+        "username": username,
+        "role": "user",
+    }
+
     return Response({"message": "register new user"}, status=status.HTTP_201_CREATED)
 
 # logs user in and returns token
