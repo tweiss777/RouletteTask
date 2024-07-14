8edd050ff941b2d7753ba34c78f812476fc81b1a
Tal Weiss <Tweiss747@gmail.com>
2024-07-14 13:42:07 -0400

    Refactored Users model to have it work with auth

--- server/server/settings.py
+++ server/server/settings.py
@@ -15,7 +15,7 @@ from datetime import timedelta
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 STATIC_URL = '/static/'
-
+AUTH_USER_MODEL = 'Users.Users'
 # Define the STATIC_ROOT directory
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
