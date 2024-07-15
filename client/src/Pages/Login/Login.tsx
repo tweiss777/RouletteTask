b64932f5c8ee2042bdf7a848366c23023f0edd25
Tal Weiss <Tweiss747@gmail.com>
2024-07-15 19:50:01 -0400

    styled login page and added register link

--- client/src/Pages/Login/Login.tsx
+++ client/src/Pages/Login/Login.tsx
@@ -1,6 +1,6 @@
 import CredentialsDTO from "../../DTOS/credentialsDTO";
 import LoginForm from "./Components/LoginForm";
-
+import "./scss/login.scss";
 export default function Login() {
   function login(credentials: CredentialsDTO) {
     console.log(credentials);
@@ -10,9 +10,12 @@ export default function Login() {
     <div className="login-page-container">
       <div className="login-form-panel">
         <div className="login-page-title">
-          <h1>Login</h1>
+          <h1>Notes App.</h1>
         </div>
-        <LoginForm handleSubmit={login} />
+        <div className="login-form-container">
+          <LoginForm handleSubmit={login} />
+            <a href="/register">Register</a>
+        </div>
       </div>
       <div className="login-background-panel"></div>
     </div>
