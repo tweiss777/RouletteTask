dc854538755a371a9f92d0c04a99512ca463570d
Tal Weiss <Tweiss747@gmail.com>
2024-07-14 20:30:35 -0400

    extracted logic for retrieving role into separate function

--- server/Auth/auth.py
+++ server/Auth/auth.py
@@ -4,40 +4,52 @@ from rest_framework_simplejwt.tokens import UntypedToken
 from rest_framework_simplejwt.exceptions import InvalidToken, TokenError
 from django.contrib.auth.models import User
 from rest_framework_simplejwt.authentication import JWTAuthentication
+def getRole(token):
+    untoken = UntypedToken(token)
+
+    jwt_auth = JWTAuthentication()
+    validated_token = jwt_auth.get_validated_token(token)
+    user = jwt_auth.get_user(validated_token)
+    role = validated_token.get('role', None)
+    return role
+
+
 class IsAuthorizedPermission(permissions.BasePermission):
     def has_permission(self, request, view):
         
-        # Get the JWT token from the Authorization header
-        auth_header = request.headers.get('Authorization', None)
-        
-        if not auth_header:
-            return False
-
         try:
-            # Remove 'Bearer' prefix if it exists
+            auth_header = request.headers.get('Authorization', None)
+        
+            if not auth_header:
+                return False
             token = auth_header.split(' ')[1]
-            
-            # Decode the JWT token
-            untoken = UntypedToken(token)
+            role: str = getRole(token) 
 
-            # Get the payload
-            jwt_auth = JWTAuthentication()
-            validated_token = jwt_auth.get_validated_token(token)
-            user = jwt_auth.get_user(validated_token)
-
-            # Extract the role claim
-            role = validated_token.get('role', None)
-            print(f'Role: {role}')
-
-            # Allow access if role is user or admin
             if role in ['user', 'admin']:
                 return True
             else:
                 return False
         except (IndexError, InvalidToken, TokenError) as e:
-            print(f'Token error: {e}')
             return False
         
         return False
+
+
 class IsAdminPermission(permissions.BasePermission):
-    pass
+    def has_permission(self, request, view):
+        try:
+            auth_header = request.headers.get('Authorization', None)
+        
+            if not auth_header:
+                return False
+            token = auth_header.split(' ')[1]
+            role: list = getRole(token) 
+
+            if role == 'admin':
+                return True
+            else:
+                return False
+        except (IndexError, InvalidToken, TokenError) as e:
+            return False
+        
+        return False
