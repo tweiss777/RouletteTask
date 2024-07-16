bf547ff2669c81aad7082da06c6224a0ed3877d0
Tal Weiss <Tweiss747@gmail.com>
2024-07-16 01:01:42 -0400

    Implemented registration service

--- client/src/Services/user.service.ts
+++ client/src/Services/user.service.ts
@@ -2,6 +2,9 @@ import CredentialsDTO from "../DTOS/credentials.dto";
 import UnauthorizedException from "../Errors/unauthorized.exception";
 import InternalServerError from "../Errors/internal.exception";
 import axios from "axios";
+import RegistrationDTO from "../DTOS/registration.dto";
+import ConflictException from "../Errors/conflict.exception";
+import BadRequestException from "../Errors/badRequest.exception";
 export async function loginUser(credentials: CredentialsDTO) {
     try {
         const response = await axios.post("/api/v1/users/login", credentials);
@@ -11,5 +14,25 @@ export async function loginUser(credentials: CredentialsDTO) {
             throw new UnauthorizedException(error.response.data.message);
         }
         throw new InternalServerError("Something went wrong");
+    }
+}
+
+export async function registerUser(registrationForm: RegistrationDTO) {
+    try {
+        console.log(registrationForm);
+        const response = await axios.post("/api/v1/users/register", {
+            ...registrationForm,
+            confirm_password: registrationForm.confirmPassword,
+        });
+        return response.data;
+    } catch (error: any) {
+        switch (error.response.status) {
+            case 409:
+                throw new ConflictException(error.response.data.message);
+            case 400:
+                throw new BadRequestException(error.response.data.message);
+            default:
+                throw new InternalServerError("Something went wrong");
+        }
     }
 }
