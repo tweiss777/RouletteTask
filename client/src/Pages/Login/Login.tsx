47aa7f4f43013d0b9d9a483994e2ae1018047f9d
Tal Weiss <Tweiss747@gmail.com>
2024-07-15 22:29:33 -0400

    cleaned up login page.

--- client/src/Pages/Login/Login.tsx
+++ client/src/Pages/Login/Login.tsx
@@ -1,23 +1,25 @@
-import CredentialsDTO from "../../DTOS/credentialsDTO";
 import LoginForm from "./Components/LoginForm";
+import useLogin from "./hooks/login";
 import "./scss/login.scss";
 export default function Login() {
-  function login(credentials: CredentialsDTO) {
-    console.log(credentials);
-  }
+    const { disableLoginBtn, error, login } = useLogin();
 
-  return (
-    <div className="login-page-container">
-      <div className="login-form-panel">
-        <div className="login-page-title">
-          <h1>Notes App.</h1>
+    return (
+        <div className="login-page-container">
+            <div className="login-form-panel">
+                <div className="login-page-title">
+                    <h1>Notes App.</h1>
+                </div>
+                <div className="login-form-container">
+                    <LoginForm
+                        errorMessage={error}
+                        disableLoginBtn={disableLoginBtn}
+                        handleSubmit={login}
+                    />
+                    
+                </div>
+            </div>
+            <div className="login-background-panel"></div>
         </div>
-        <div className="login-form-container">
-          <LoginForm handleSubmit={login} />
-            <a href="/register">Register</a>
-        </div>
-      </div>
-      <div className="login-background-panel"></div>
-    </div>
-  );
+    );
 }
