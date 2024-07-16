5c5d9112a03f0e3470fc236a8d62f52263e4e1b1
Tal Weiss <Tweiss747@gmail.com>
2024-07-16 01:01:23 -0400

    refactors and implemented register

--- client/src/Pages/Login/hooks/login.ts
+++ client/src/Pages/Login/hooks/login.ts
@@ -2,18 +2,20 @@ import { useState } from "react";
 import CredentialsDTO from "../../../DTOS/credentials.dto";
 import UnauthorizedException from "../../../Errors/unauthorized.exception";
 import useAuthContext from "../../../hooks/useAuthContext";
-import { loginUser } from "../../../Services/user.service";
+import { loginUser, registerUser } from "../../../Services/user.service";
 import RegistrationDTO from "../../../DTOS/registration.dto";
+import ConflictException from "../../../Errors/conflict.exception";
+import BadRequestException from "../../../Errors/badRequest.exception";
 
 export default function useLogin() {
-    const [disableBtn, setDisableLoginBtn] = useState(false);
+    const [disableBtn, setDisableBtn] = useState(false);
     const [error, setError] = useState<string>("");
     const { setUserData }  = useAuthContext();
     async function login(credentials: CredentialsDTO) {
         try {
             if (error) setError("");
             const data = await loginUser(credentials);
-            setDisableLoginBtn(true);
+            setDisableBtn(true);
             if (data?.token) {
                 const token = data.token;
                 setUserData(token);
@@ -26,12 +28,29 @@ export default function useLogin() {
             setError("Something went wrong");
             
         } finally {
-            setDisableLoginBtn(false);
+            setDisableBtn(false);
         }
     }
 
-    function register(registrationForm: RegistrationDTO) {
-        throw new Error("Function not implemented.");
+    async function register(registrationForm: RegistrationDTO) {
+        try {
+            setDisableBtn(true); 
+            if (error) setError("");
+            const data = await registerUser(registrationForm);
+            if (data?.token) {
+            const token = data.token;
+                setUserData(token);
+            }
+        } catch (error: any) {
+           if(error instanceof ConflictException || error instanceof BadRequestException) {
+               setError(error.message); 
+                return
+           } 
+            setError("Something went wrong");
+        } finally{
+            setDisableBtn(false);
+
+        }
 
     }
 
