13b1ebbe8d6b5b1e351e0a71704d766508889671
Tal Weiss <Tweiss747@gmail.com>
2024-07-15 23:23:10 -0400

    pass token to context

--- client/src/Pages/Login/hooks/login.ts
+++ client/src/Pages/Login/hooks/login.ts
@@ -1,16 +1,22 @@
 import { useState } from "react";
 import CredentialsDTO from "../../../DTOS/credentials.dto";
 import UnauthorizedException from "../../../Errors/unauthorized.exception";
+import useAuthContext from "../../../hooks/useAuthContext";
 import { loginUser } from "../../../Services/user.service";
 
 export default function useLogin() {
     const [disableLoginBtn, setDisableLoginBtn] = useState(false);
     const [error, setError] = useState<string>("");
+    const { setUserData }  = useAuthContext();
     async function login(credentials: CredentialsDTO) {
         try {
             if (error) setError("");
             const data = await loginUser(credentials);
             setDisableLoginBtn(true);
+            if (data?.token) {
+                const token = data.token;
+                setUserData(token);
+            }
         } catch (error) {
             if (error instanceof UnauthorizedException) {
                 setError(error.message);
