c9bc0dce538c1aa0790bd84955377cfa743ed2fb
Tal Weiss <Tweiss747@gmail.com>
2024-07-16 01:01:39 -0400

    created registration form

--- client/src/Pages/Login/Components/RegistrationForm.tsx
+++ client/src/Pages/Login/Components/RegistrationForm.tsx
@@ -3,74 +3,75 @@ import RegistrationDTO from "../../../DTOS/registration.dto";
 import { useState } from "react";
 import "../scss/registrationForm.scss";
 interface IProps {
-  handleRegistration: (loginForm: RegistrationDTO) => void;
-  disableRegistrationBtn?: boolean;
-  errorMessage?: string;
+    handleRegistration: (loginForm: RegistrationDTO) => void;
+    disableRegistrationBtn?: boolean;
+    errorMessage?: string;
 }
 
 export default function RegistrationForm({
-  handleRegistration,
-  errorMessage,
-  disableRegistrationBtn,
+    handleRegistration,
+    errorMessage,
+    disableRegistrationBtn,
 }: IProps) {
-  const [registrationForm, setRegistrationForm] = useState<RegistrationDTO>({
-    username: "",
-    password: "",
-    confirmPassword: "",
-  });
-
-  function handleOnChange(e: React.ChangeEvent<HTMLInputElement>) {
-    setRegistrationForm({
-      [e.target.name]: e.target.value,
-      ...registrationForm,
+    const [registrationForm, setRegistrationForm] = useState({
+        username: "",
+        password: "",
+        confirmPassword: "",
     });
-  }
 
-  function register(e: React.FormEvent<HTMLFormElement>) {
-    e.preventDefault();
-    handleRegistration(registrationForm);
-  }
-  return (
-    <div className="registration-form">
-      <form onSubmit={register}>
-        {errorMessage && <div className="error-message">{errorMessage}</div>}
-        <div className="username-field">
-          <label>Username</label>
-          <input
-            onChange={handleOnChange}
-            type="text"
-            name="username"
-            value={registrationForm.username}
-          />
-        </div>
+    function handleOnChange(e: React.ChangeEvent<HTMLInputElement>) {
+        console.log(e.target.name)
+        setRegistrationForm({
+            ...registrationForm,
+            [e.target.name]: e.target.value,
+        });
+    }
 
-        <div className="password-field">
-          <label>Password</label>
-          <input
-            onChange={handleOnChange}
-            type="password"
-            name="password"
-            value={registrationForm.password}
-          />
+    function register(e: React.FormEvent<HTMLFormElement>) {
+        e.preventDefault();
+        handleRegistration(registrationForm);
+    }
+    return (
+        <div className="registration-form">
+            <form onSubmit={register}>
+                {errorMessage && <div className="error-message">{errorMessage}</div>}
+                <div className="username-field">
+                    <label>Username</label>
+                    <input
+                        onChange={handleOnChange}
+                        type="text"
+                        name="username"
+                        value={registrationForm.username}
+                    />
+                </div>
+
+                <div className="password-field">
+                    <label>Password</label>
+                    <input
+                        onChange={handleOnChange}
+                        type="password"
+                        name="password"
+                        value={registrationForm.password}
+                    />
+                </div>
+                <div className="cofirm-password-field">
+                    <label>Confirm Password</label>
+                    <input
+                        onChange={handleOnChange}
+                        type="password"
+                        name="confirmPassword"
+                        value={registrationForm.confirmPassword}
+                    />
+                </div>
+                <div className="register-button">
+                    <input
+                        disabled={disableRegistrationBtn}
+                        type="submit"
+                        value="Register"
+                    />
+                </div>
+            </form>
+            <Link to="/login">Login</Link>
         </div>
-        <div className="cofirm-password-field">
-          <label>Confirm Password</label>
-          <input
-            onChange={handleOnChange}
-            type="password"
-            name="confirmPassword"
-            value={registrationForm.password}
-          />
-        </div>
-        <div className="register-button">
-          <input
-            disabled={disableRegistrationBtn}
-            type="submit"
-            value="Register"
-          />
-        </div>
-      </form>
-      <Link to="/login">Login</Link>
-    </div>
-  );
+    );
 }
