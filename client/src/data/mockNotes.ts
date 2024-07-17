4afba67b0f95b8bb04c082e56bfa01dbb9dde8a8
Tal Weiss <Tweiss747@gmail.com>
2024-07-16 23:32:52 -0400

    implemented dashboard

---
+++ client/src/data/mockNotes.ts
@@ -0,0 +1,38 @@
+const mockNotes = [
+  {
+    id: "1",
+    title: "Note 1",
+    content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, voluptate."
+  },
+  {
+    id: "2",
+    title: "Note 2",
+    content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, voluptate."
+  },
+  {
+    id: "3",
+    title: "Note 3",
+    content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, voluptate."
+  },
+  {
+    id: "4",
+    title: "Note 4",
+    content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, voluptate."
+  },
+  {
+    id: "5",
+    title: "Note 5",
+    content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, voluptate."
+  },
+  {
+    id: "6",
+    title: "Note 6",
+    content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, voluptate."
+  },
+  {
+    id: "7",
+    title: "Note 7",
+    content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, voluptate."
+  }
+];
+export default mockNotes
