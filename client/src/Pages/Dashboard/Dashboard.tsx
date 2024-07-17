1aeac104f1f904a9bf0b6cba0cf647ae60958398
Tal Weiss <Tweiss747@gmail.com>
2024-07-16 22:04:30 -0400

    injected note component to page.

--- client/src/Pages/Dashboard/Dashboard.tsx
+++ client/src/Pages/Dashboard/Dashboard.tsx
@@ -1,5 +1,10 @@
 import './scss/Dashboad.scss';
+import NoteForm from './Components/NewNoteForm';
 export default function Dashboard() {
+  function createNewNote(title: string, content: string) {
+    throw new Error('Function not implemented.');
+  }
+
   return (
     <div className="dashboard-container">
       <div className="side-panel">
@@ -8,7 +13,7 @@ export default function Dashboard() {
 
       <div className="main-panel">
         <div className="notes-text-container">
-          <h1>Create Note</h1>
+          <NoteForm handleSubmit={createNewNote} />
         </div>
       </div>
     </div>
